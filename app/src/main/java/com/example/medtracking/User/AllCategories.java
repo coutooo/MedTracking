b8d4250138ff75b2d9e18dd83e1065501c847a3d
Couto <manuelcouto10@ua.pt>
2022-05-31 18:41:31 +0100

    sadsa

---
+++ app/src/main/java/com/example/medtracking/User/AllCategories.java
@@ -0,0 +1,16 @@
+package com.example.medtracking.User;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.medtracking.R;
+
+public class AllCategories extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_categories);
+    }
+}
