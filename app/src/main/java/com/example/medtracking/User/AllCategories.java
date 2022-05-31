7bb48b10c738939e7ff11beac5feea0555b63478
Couto <manuelcouto10@ua.pt>
2022-06-01 00:01:16 +0100

    UI done first page

--- app/src/main/java/com/example/medtracking/User/AllCategories.java
+++ app/src/main/java/com/example/medtracking/User/AllCategories.java
@@ -3,14 +3,28 @@ package com.example.medtracking.User;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.example.medtracking.R;
 
 public class AllCategories extends AppCompatActivity {
 
+    ImageView backBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_categories);
+
+        //hooks
+        backBtn = findViewById(R.id.back_pressed);
+
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AllCategories.super.onBackPressed();
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/medtracking/User/UserDashBoard.java
@@ -0,0 +1,15 @@
+package com.example.medtracking.User;
+
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+
+
+public class UserDashBoard {
+
+    public void onBackPressed(){}
+
+    public boolean onNavigationItemSelected(@NonNull MenuItem item){
+        return true;
+    }
+}
