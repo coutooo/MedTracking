139394021ff87f2d9e7b27853015e4d4e1244911
Couto <manuelcouto10@ua.pt>
2022-06-20 17:24:33 +0100

    start tasks page

---
+++ phone/app/src/main/java/com/example/medtracking/User/CheckBoxActivity.java
@@ -0,0 +1,207 @@
+package com.example.medtracking.User;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.medtracking.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CheckBoxActivity extends AppCompatActivity {
+    public class Item {
+        boolean checked;
+        Drawable ItemDrawable;
+        String ItemString;
+        Item(Drawable drawable, String t, boolean b){
+            ItemDrawable = drawable;
+            ItemString = t;
+            checked = b;
+        }
+
+        public boolean isChecked(){
+            return checked;
+        }
+    }
+
+    static class ViewHolder {
+        CheckBox checkBox;
+        ImageView icon;
+        TextView text;
+    }
+
+    public class ItemsListAdapter extends BaseAdapter {
+
+        private Context context;
+        private List<Item> list;
+
+        ItemsListAdapter(Context c, List<Item> l) {
+            context = c;
+            list = l;
+        }
+
+        @Override
+        public int getCount() {
+            return list.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return list.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        public boolean isChecked(int position) {
+            return list.get(position).checked;
+        }
+
+        @Override
+        public View getView(final int position, View convertView, ViewGroup parent) {
+            View rowView = convertView;
+
+            // reuse views
+            ViewHolder viewHolder = new ViewHolder();
+            if (rowView == null) {
+                LayoutInflater inflater = ((Activity) context).getLayoutInflater();
+                rowView = inflater.inflate(R.layout.row, null);
+
+                viewHolder.checkBox = (CheckBox) rowView.findViewById(R.id.rowCheckBox);
+                viewHolder.icon = (ImageView) rowView.findViewById(R.id.rowImageView);
+                viewHolder.text = (TextView) rowView.findViewById(R.id.rowTextView);
+                rowView.setTag(viewHolder);
+            } else {
+                viewHolder = (ViewHolder) rowView.getTag();
+            }
+
+            viewHolder.icon.setImageDrawable(list.get(position).ItemDrawable);
+            viewHolder.checkBox.setChecked(list.get(position).checked);
+
+            final String itemStr = list.get(position).ItemString;
+            viewHolder.text.setText(itemStr);
+
+            viewHolder.checkBox.setTag(position);
+
+            /*
+            viewHolder.checkBox.setOnCheckedChangeListener(
+                    new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                    list.get(position).checked = b;
+
+                    Toast.makeText(getApplicationContext(),
+                            itemStr + "onCheckedChanged\nchecked: " + b,
+                            Toast.LENGTH_LONG).show();
+                }
+            });
+            */
+
+            viewHolder.checkBox.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    boolean newState = !list.get(position).isChecked();
+                    list.get(position).checked = newState;
+                    Toast.makeText(getApplicationContext(),
+                            itemStr + "setOnClickListener\nchecked: " + newState,
+                            Toast.LENGTH_LONG).show();
+                }
+            });
+
+            viewHolder.checkBox.setChecked(isChecked(position));
+
+            return rowView;
+        }
+    }
+
+    Button btnLookup;
+    List<Item> items;
+    ListView listView;
+    ItemsListAdapter myItemsListAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        listView = (ListView)findViewById(R.id.listviewChecks);
+        btnLookup = (Button)findViewById(R.id.lookup);
+
+        initItems();
+        myItemsListAdapter = new ItemsListAdapter(this, items);
+        listView.setAdapter(myItemsListAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                Toast.makeText(CheckBoxActivity.this,
+                        ((Item)(parent.getItemAtPosition(position))).ItemString,
+                        Toast.LENGTH_LONG).show();
+            }});
+
+        btnLookup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String str = "Check items:\n";
+
+                for (int i=0; i<items.size(); i++){
+                    if (items.get(i).isChecked()){
+                        str += i + "\n";
+                    }
+                }
+
+                /*
+                int cnt = myItemsListAdapter.getCount();
+                for (int i=0; i<cnt; i++){
+                    if(myItemsListAdapter.isChecked(i)){
+                        str += i + "\n";
+                    }
+                }
+                */
+
+                Toast.makeText(CheckBoxActivity.this,
+                        str,
+                        Toast.LENGTH_LONG).show();
+
+            }
+        });
+    }
+
+    private void initItems(){
+        items = new ArrayList<Item>();
+
+        TypedArray arrayDrawable = getResources().obtainTypedArray(R.array.resicon);
+        TypedArray arrayText = getResources().obtainTypedArray(R.array.restext);
+
+        for(int i=0; i<arrayDrawable.length(); i++){
+            Drawable d = arrayDrawable.getDrawable(i);
+            String s = arrayText.getString(i);
+            boolean b = false;
+            Item item = new Item(d, s, b);
+            items.add(item);
+        }
+
+        arrayDrawable.recycle();
+        arrayText.recycle();
+    }
+
+}
--- phone/app/src/main/java/com/example/medtracking/User/TarefaActivity.java
+++ phone/app/src/main/java/com/example/medtracking/User/TarefaActivity.java
@@ -2,10 +2,13 @@ package com.example.medtracking.User;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.medtracking.MainActivity;
 import com.example.medtracking.R;
 import com.example.medtracking.databinding.ActivityTarefasBinding;
 
@@ -13,11 +16,24 @@ public class TarefaActivity extends AppCompatActivity {
 
     ActivityTarefasBinding binding;
 
+    Button btnTasks;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityTarefasBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        btnTasks = findViewById(R.id.btnTasks);
+
+        btnTasks.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent i = new Intent(TarefaActivity.this, CheckBoxActivity.class);
+                        startActivity(i);
+                    }
+                }
+        );
 
         Intent intent = this.getIntent();
 
@@ -32,9 +48,6 @@ public class TarefaActivity extends AppCompatActivity {
             binding.nameAndCause.setText(nameAndCause);
             binding.profileImage.setImageResource(imageId);
             binding.inAndOut.setText(inAndout);
-
-
-
         }
     }
 }
